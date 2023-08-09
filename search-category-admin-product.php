8bbeddb39f7f1a2b9f0d79e2e94666cc4282a843
Davide Ladisa <32127923+FrancoStino@users.noreply.github.com>
2023-08-09 12:53:13 +0200

    Update search-category-admin-product.php

--- search-category-admin-product.php
+++ search-category-admin-product.php
@@ -1,11 +1,26 @@
 <?php
 
-add_action('admin_enqueue_scripts', function() {
-	
-	wp_enqueue_script( 'pfw-admin', get_stylesheet_directory_uri() . '/js/pcswc-admin.js', array('jquery'), filemtime(get_stylesheet_directory_uri() . '/js/pcswc-admin.js'), true);
+// Definiamo una funzione per l'inclusione degli script nell'area di amministrazione
+function pfw_enqueue_admin_scripts() {
+    // Otteniamo il percorso completo dello script JavaScript pcswc-admin.js nel tema figlio
+    $script_path = get_stylesheet_directory_uri() . '/search-category-admin-product/js/pcswc-admin.js';
+    
+    // Otteniamo il timestamp dell'ultima modifica dello script pcswc-admin.js
+    // Utilizziamo il percorso del file locale, non l'URL, con la funzione filemtime()
+    $script_mtime = filemtime(get_stylesheet_directory() . '/search-category-admin-product/js/pcswc-admin.js');
 
-    wp_localize_script( 'pfw-admin', 'main_vars', array(
+    // Includiamo lo script pfw-admin, specificando dipendenze come 'jquery'
+    // Utilizziamo il percorso e il timestamp dell'ultima modifica dello script
+    // Impostiamo il caricamento nello footer (parametro true) per migliorare le prestazioni
+    wp_enqueue_script('pfw-admin', $script_path, array('jquery'), $script_mtime, true);
+
+    // Localizziamo alcune variabili per l'utilizzo nello script
+    // In questo caso, stiamo localizzando la stringa 'Cerca' per lo script
+    wp_localize_script('pfw-admin', 'main_vars', array(
         'search' => __('Cerca', 'pcswc'),
     ));
+}
 
-}, 100);
+// Aggiungiamo l'azione per chiamare la nostra funzione di inclusione degli script nell'area di amministrazione
+// Utilizziamo la priorità 100 per garantire che la funzione venga eseguita dopo altre possibili inclusione di script
+add_action('admin_enqueue_scripts', 'pfw_enqueue_admin_scripts', 100);
